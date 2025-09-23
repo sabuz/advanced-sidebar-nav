d7ecfe7b69a9e3272969a8d648e9e9036e6ea3b9
Nazmul Islam Sabuz <sabuz@users.noreply.github.com>
2025-09-23 20:01:56 +0600

    wip structure

--- advanced-sidebar-nav.php
+++ advanced-sidebar-nav.php
@@ -9,85 +9,68 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
-final class Advanced_Sidebar_Nav
-{
-    protected static $instance = null;
+final class Advanced_Sidebar_Nav {
 
-    protected function __construct()
-    {
-        // methods
-        $this->load_files();
+	protected static $instance = null;
 
-        // actions
-        add_action('wp_enqueue_scripts', array($this, 'enqueue_scripts'));
-        add_action('widgets_init', array($this, 'register_widget'));
-    }
+	protected function __construct() {
+		// methods
+		$this->load_files();
 
-    // create instance
-    public static function instance()
-    {
-        if (self::$instance == null) {
-            $instance = new self;
-        }
-
-        return $instance;
-    }
-
-    // load required files
-    public function load_files()
-    {
-        require_once plugin_dir_path(__FILE__) . 'inc/options.php';
-        require_once plugin_dir_path(__FILE__) . 'inc/widget.php';
-    }
-
-    // register assets
-    public function enqueue_scripts()
-    {
-        wp_register_style('advanced-sidebar-nav', plugin_dir_url(__FILE__) . 'assets/advanced-sidebar-nav.css');
-        wp_register_script('advanced-sidebar-nav', plugin_dir_url(__FILE__) . 'assets/advanced-sidebar-nav.js');
-    }
-
-    // register wp widget
-    public function register_widget()
-    {
-        register_widget('Advanced_Sidebar_Nav_Widget');
-    }
-}
-
-add_action('plugins_loaded', function () {
-    Advanced_Sidebar_Nav::instance();
-});
-
-function create_block_advanced_sidebar_nav_block_init() {
-	/**
-	 * Registers the block(s) metadata from the `blocks-manifest.php` and registers the block type(s)
-	 * based on the registered block metadata.
-	 * Added in WordPress 6.8 to simplify the block metadata registration process added in WordPress 6.7.
-	 *
-	 * @see https://make.wordpress.org/core/2025/03/13/more-efficient-block-type-registration-in-6-8/
-	 */
-	if ( function_exists( 'wp_register_block_types_from_metadata_collection' ) ) {
-		wp_register_block_types_from_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
-		return;
+		// actions
+		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
+		add_action( 'widgets_init', [ $this, 'register_widget' ] );
+		add_action( 'init', [ $this, 'create_block_advanced_sidebar_nav_block_init' ] );
 	}
 
-	/**
-	 * Registers the block(s) metadata from the `blocks-manifest.php` file.
-	 * Added to WordPress 6.7 to improve the performance of block type registration.
-	 *
-	 * @see https://make.wordpress.org/core/2024/10/17/new-block-type-registration-apis-to-improve-performance-in-wordpress-6-7/
-	 */
-	if ( function_exists( 'wp_register_block_metadata_collection' ) ) {
-		wp_register_block_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
+	// create instance
+	public static function instance() {
+		if ( self::$instance == null ) {
+			$instance = new self();
+		}
+
+		return $instance;
 	}
-	/**
-	 * Registers the block type(s) in the `blocks-manifest.php` file.
-	 *
-	 * @see https://developer.wordpress.org/reference/functions/register_block_type/
-	 */
-	$manifest_data = require __DIR__ . '/build/blocks-manifest.php';
-	foreach ( array_keys( $manifest_data ) as $block_type ) {
-		register_block_type( __DIR__ . "/build/{$block_type}" );
+
+	// load required files
+	public function load_files() {
+		require_once plugin_dir_path( __FILE__ ) . 'inc/options.php';
+		require_once plugin_dir_path( __FILE__ ) . 'inc/widget.php';
+	}
+
+	// register assets
+	public function enqueue_scripts() {
+		wp_register_style( 'advanced-sidebar-nav', plugin_dir_url( __FILE__ ) . 'assets/advanced-sidebar-nav.css' );
+		wp_register_script( 'advanced-sidebar-nav', plugin_dir_url( __FILE__ ) . 'assets/advanced-sidebar-nav.js' );
+	}
+
+	// register wp widget
+	public function register_widget() {
+		register_widget( 'Advanced_Sidebar_Nav_Widget' );
+	}
+
+	// Block registration
+	public function create_block_advanced_sidebar_nav_block_init() {
+		if ( function_exists( 'wp_register_block_types_from_metadata_collection' ) ) {
+			wp_register_block_types_from_metadata_collection(
+				__DIR__ . '/blocks/advanced-sidebar-nav/build',
+				__DIR__ . '/blocks/advanced-sidebar-nav/build/blocks-manifest.php'
+			);
+			return;
+		}
+
+		if ( function_exists( 'wp_register_block_metadata_collection' ) ) {
+			wp_register_block_metadata_collection(
+				__DIR__ . '/blocks/advanced-sidebar-nav/build',
+				__DIR__ . '/blocks/advanced-sidebar-nav/build/blocks-manifest.php'
+			);
+		}
+
+		$manifest_data = require __DIR__ . '/blocks/advanced-sidebar-nav/build/blocks-manifest.php';
+		foreach ( array_keys( $manifest_data ) as $block_type ) {
+			register_block_type( __DIR__ . "/blocks/advanced-sidebar-nav/build/{$block_type}" );
+		}
 	}
 }
-add_action( 'init', 'create_block_advanced_sidebar_nav_block_init' );
+
+Advanced_Sidebar_Nav::instance();
---
+++ blocks/advanced-sidebar-nav/render.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+
+// Extract attributes
+$title       = $attributes['title'] ?? '';
+$menu        = $attributes['menu'] ?? '';
+$theme       = $attributes['theme'] ?? 'default';
+$accentColor = $attributes['accentColor'] ?? '#0f434f';
+
+error_log( print_r( $attributes, true ) );
+
+// Build CSS classes
+$classes            = [ 'advanced-sidebar-nav', 'advanced-sidebar-nav-' . $theme ];
+$wrapper_attributes = get_block_wrapper_attributes(
+	[
+		'class' => implode( ' ', $classes ),
+		'style' => ! empty( $accentColor ) ? '--accent-color: ' . esc_attr( $accentColor ) . ';' : '',
+	]
+);
+?>
+
+<div <?php echo $wrapper_attributes; ?>>
+	<?php if ( ! empty( $title ) ) : ?>
+		<h3 class="advanced-sidebar-nav-title"><?php echo esc_html( $title ); ?></h3>
+	<?php endif; ?>
+
+	<?php if ( ! empty( $menu ) ) : ?>
+		<?php
+		$nav_menu = wp_nav_menu(
+			[
+				'menu'            => $menu,
+				'menu_class'      => 'advanced-sidebar-menu',
+				'container_class' => 'advanced-sidebar-nav-container',
+				'echo'            => false,
+			]
+		);
+
+		if ( $nav_menu ) {
+			echo $nav_menu;
+		} else {
+			echo '<p>' . __( 'No menu found. Please select a valid menu.', 'advanced-sidebar-nav' ) . '</p>';
+		}
+		?>
+	<?php else : ?>
+		<p><?php _e( 'Please select a menu from the block settings.', 'advanced-sidebar-nav' ); ?></p>
+	<?php endif; ?>
+</div>
