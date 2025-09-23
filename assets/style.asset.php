825b0e58c49c2a08c570498be85661efa179d77f
Nazmul Islam Sabuz <sabuz@users.noreply.github.com>
2025-09-23 22:02:14 +0600

    wip

--- assets/style.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '3d77313384ad34b71e7f');
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ module.exports = {
 	entry: {
 		'block/build/index': './block/src/index.js',
 		'block/build/view': './block/src/view.js',
-		'assets/style': './assets/style.scss',
 	},
 	output: {
 		path: __dirname,
