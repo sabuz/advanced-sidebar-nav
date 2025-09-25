e1d99ca70f91e25320c277377c68bc98b32ea8b7
Nazmul Islam Sabuz <sabuz@users.noreply.github.com>
2025-09-25 21:00:14 +0600

    add build in git

---
+++ block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a6e4002d5344f73da0b3');
